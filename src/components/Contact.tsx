cb2bd9f97c4ece8584125583179ee35939ef8467
baristrzx <167886091+baristrzx@users.noreply.github.com>
2026-02-26 18:44:00 +0300

    Updated Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -89,7 +89,7 @@ export default function Contact() {
           >
             <iframe
               title="ANKH Legal Konum"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12757.673099295898!2d29.1048!3d36.6218!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14c044f9b9df5de5%3A0xb5cd53b75fba4cb7!2sFethiye%2C%20Mu%C4%9Fla!5e0!3m2!1str!2str!4v1700000000000!5m2!1str!2str"
+              src="https://maps.google.com/maps?q=Fo%C3%A7a+Mahallesi+983.+Sokak+No:18%2F2+Fethiye+Mu%C4%9Fla&t=&z=16&ie=UTF8&iwloc=&output=embed"
               width="100%"
               height="100%"
               style={{ border: 0, minHeight: '420px' }}
